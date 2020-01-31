bc1ec2cec788b3f36266d319f6ef08d54390609a
a2300 <49100630+a2300@users.noreply.github.com>
2020-01-31 18:02:04 +0200

    Create Events.java

---
+++ src/main/java/com/ybuy/statemachine/statemachine/Events.java
@@ -0,0 +1,11 @@
+package com.ybuy.statemachine.statemachine;
+
+public enum Events {
+    START_FEATURE,
+    FINISH_FEATURE,
+    QA_TEAM_REJECT,
+    ROCK_STAR_MAKE_ALL_IN_ONE,
+    DEPLOY,
+    QA_TEAM_APPROVE
+}
+
